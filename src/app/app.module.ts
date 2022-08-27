bfb58970874802f0160e7dde0648ef73bfcee860
Alexey Kurgan <akurgan880@gmail.com>
2022-08-27 21:45:51 +0300

    MDB-2 "add material and ngx-bootstrap libs and basic components"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { PageContentComponent } from './components/page-content/page-content.component';
+import { MovieCardComponent } from './components/page-content/movie-card/movie-card.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent,
+    PageContentComponent,
+    MovieCardComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,41 @@
+// import modules
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SharedComponent } from './shared.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// ngx-bootstrap modules
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { ButtonsModule } from 'ngx-bootstrap/buttons';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
+
+// material modules
+import {MatPaginatorModule} from '@angular/material/paginator';
+
+// declarations components
+import { MainSliderComponent } from './components/main-slider/main-slider.component';
+import { ButtonComponent } from './components/button/button.component';
+import { PaginatorComponent } from './components/paginator/paginator.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    BrowserAnimationsModule,
+    // ngx-bootstrap
+    TooltipModule.forRoot(),
+    CarouselModule.forRoot(),
+    ButtonsModule.forRoot(),
+    // material
+    MatPaginatorModule,
   ],
-  declarations: [SharedComponent]
+  declarations:[
+    MainSliderComponent,
+    ButtonComponent,
+    PaginatorComponent,
+  ],
+  exports:[
+    MainSliderComponent,
+    ButtonComponent,
+    PaginatorComponent,
+  ]
 })
 export class SharedModule { }
