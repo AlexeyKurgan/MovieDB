eb947df1f89d0d4754f3f5b4587ba17f3ff95743
Alexey Kurgan <akurgan880@gmail.com>
2022-08-28 04:32:50 +0300

    MDB-3 "header component markup"

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -10,11 +10,19 @@ import { CarouselModule } from 'ngx-bootstrap/carousel';
 
 // material modules
 import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatMenuModule} from '@angular/material/menu';
 
 // declarations components
 import { MainSliderComponent } from './components/main-slider/main-slider.component';
 import { ButtonComponent } from './components/button/button.component';
 import { PaginatorComponent } from './components/paginator/paginator.component';
+import { SearchComponent } from './components/search/search.component';
+import { UserMenuComponent } from './components/user-menu/user-menu.component';
 
 @NgModule({
   imports: [
@@ -26,16 +34,26 @@ import { PaginatorComponent } from './components/paginator/paginator.component';
     ButtonsModule.forRoot(),
     // material
     MatPaginatorModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatSelectModule,
+    MatChipsModule,
+    MatMenuModule,
   ],
   declarations:[
     MainSliderComponent,
     ButtonComponent,
     PaginatorComponent,
+    SearchComponent,
+    UserMenuComponent,
   ],
   exports:[
     MainSliderComponent,
     ButtonComponent,
     PaginatorComponent,
+    SearchComponent,
+    UserMenuComponent,
   ]
 })
 export class SharedModule { }
