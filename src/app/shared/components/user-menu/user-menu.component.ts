58c7901fbcd24e0dde5de6fc31e5203b6b283233
Alexey Kurgan <akurgan880@gmail.com>
2022-08-28 17:07:25 +0300

    MDB-3 "header component markup"

--- src/app/shared/components/user-menu/user-menu.component.ts
+++ src/app/shared/components/user-menu/user-menu.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-user-menu',
   templateUrl: './user-menu.component.html',
-  styleUrls: ['./user-menu.component.scss']
 })
 export class UserMenuComponent implements OnInit {
 
