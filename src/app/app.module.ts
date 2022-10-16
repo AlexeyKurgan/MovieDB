7de42dae0445650ab13d3a6b19ed0dc45c873dcf
AlexeyKurgan <48919708+AlexeyKurgan@users.noreply.github.com>
2022-10-16 18:26:21 +0300

    MDB-7 upcoming component markup (#8)
    Co-authored-by: Alexey Kurgan <akurgan880@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+
 import { CommonModule } from './common/common.module';
+import { SharedModule } from './shared/shared.module'; 
 import { LandingDashboardModule } from './landing-dashboard/landing-dashboard.module';
 import { UserManagmentModule } from './user-managment/user-managment.module';
 
-// check this module why he didn't work in app module
-import { SharedModule } from './shared/shared.module'; 
+// components
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
